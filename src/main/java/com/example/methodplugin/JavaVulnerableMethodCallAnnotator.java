d0429c7870c9ef1eef7223a3491f7f902674e735
Jimmy Chen <49624964+Qiming-C@users.noreply.github.com>
2023-04-25 22:00:23 -0400

    prototype

--- src/main/java/com/example/methodplugin/JavaVulnerableMethodCallAnnotator.java
+++ src/main/java/com/example/methodplugin/JavaVulnerableMethodCallAnnotator.java
@@ -10,6 +10,7 @@ import com.intellij.openapi.util.TextRange;
 import com.intellij.psi.PsiElement;
 import com.intellij.psi.PsiMethod;
 import com.intellij.psi.PsiMethodCallExpression;
+import com.intellij.psi.PsiSubstitutor;
 import org.jetbrains.annotations.NotNull;
 
 public class JavaVulnerableMethodCallAnnotator extends VulnerableMethodCallAnnotator
@@ -22,15 +23,25 @@ public class JavaVulnerableMethodCallAnnotator extends VulnerableMethodCallAnnot
     if (element instanceof PsiMethodCallExpression) {
       PsiMethodCallExpression methodCallExpression = (PsiMethodCallExpression) element;
       TextRange range = methodCallExpression.getTextRange();
-      Integer severity = 1;
       PsiMethod psiMethod = methodCallExpression.resolveMethod();
-      if (psiMethod != null && vulnerableMethods.contains(psiMethod.getName())) {
-        holder.newAnnotation(HighlightSeverity.ERROR, "")
-            .tooltip("This is a tooltip")
-            .range(range)
-            .enforcedTextAttributes(VulnerableMethodCallAnnotator.HighLightError)
-            .create();
-      }
+        if (psiMethod != null && vulnerableMethods.contains(psiMethod.getName())) {
+          StringBuilder tooltip = new StringBuilder();
+          String title = "<h1>Vulnerable Method Call</h1><br/>";
+          String componentName = psiMethod.getContainingClass().getQualifiedName();
+          String methodName = psiMethod.getName();
+
+          String remediation = "<p>Remediation: Avoid using this method</p>";
+          tooltip.append(title);
+          tooltip.append("<b>Component: </b>").append(componentName).append("<br/>");
+          tooltip.append("<b>Method: </b>").append(methodName).append("<br/>");
+          tooltip.append(remediation);
+          holder.newAnnotation(HighlightSeverity.ERROR, "")
+              .tooltip(tooltip.toString())
+              .range(range)
+              .enforcedTextAttributes(VulnerableMethodCallAnnotator.HighLightError)
+              .create();
+        }
+
     }
   }
 }
